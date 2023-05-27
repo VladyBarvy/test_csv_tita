940ace9fecea23e403ee533420ecfce922b18324
root <root@vladybarvy.localdomain>
2023-05-27 17:11:27 +0300

    step 1

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+  //console.log(content);
+
+
+  // step 1
+  const info = content.split('\r\n');
+  const data = info.slice(1, info.length - 1);
+  const value_of_pass = data.length;
+  console.log(`Сколько всего пассажиров в таблице: ${value_of_pass}`);
   // END
 }
